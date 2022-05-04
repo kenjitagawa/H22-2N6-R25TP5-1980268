103790e7ce292f12769b3fbce8e4f70a1c2a36e2
Kenji Tagawa <kenjitagawa.kt@gmail.com>
2022-05-04 13:49:54 -0400

    FCT : TODO #02
    Ajout de l’interface IChanson

---
+++ BaladeurMultiFormats/IChanson.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaladeurMultiFormats
+{
+    internal interface IChanson
+    {
+        int Annee { get; }
+
+        string Artiste { get; }
+
+        string Format { get; }
+
+        string NomFichier { get; }
+
+        string Paroles { get; }
+
+        string Titre { get; }
+
+
+
+        void Ecrire(string pParoles);
+
+        void EcrireEntete(StreamReader pobjFichier);
+
+        void EcrireParoles(StreamWriter pobjFichier, string pParoles);
+
+        void LireEntete();
+
+        string LireParoles(StreamReader pobjFichier);
+
+        void SauterEntete(StreamWriter pobjFichier);
+
+
+
+
+    }
+}
