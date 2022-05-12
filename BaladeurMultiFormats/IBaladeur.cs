00cf8b56e008fc22db95522f551a2dc25845103a
Kenji Tagawa <kenjitagawa.kt@gmail.com>
2022-05-12 10:51:49 -0400

    FCT : TODO #07
    Ajout de l’interface IBaladeur

---
+++ BaladeurMultiFormats/IBaladeur.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaladeurMultiFormats
+{
+    public interface IBaladeur
+    {
+		int NbChansons { get; }
+
+		void AfficherLesChansons(ListView pListView);
+
+		Chanson ChansonAt(int pIndex);
+
+		void ConstruireLaListeDesChansons();
+
+		void ConvertirVersAAC(int pIndex);
+
+		void ConvertirVersMP3(int pIndex);
+
+		void ConvertirVersWMA(int pIndex);
+	}
+}
