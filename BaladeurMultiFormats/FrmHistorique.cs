2d0ad346baf8cd79c0fe8c1c029a4a262cefbf22
Kenji Tagawa <kenjitagawa.kt@gmail.com>
2022-05-17 10:22:29 -0400

    FCT : TODO #16
    Finish the forms in order to get them working for production.

--- BaladeurMultiFormats/FrmHistorique.cs
+++ BaladeurMultiFormats/FrmHistorique.cs
@@ -29,47 +29,47 @@ namespace BaladeurMultiFormats
         /// <param name="pHistorique">Historique à afficher</param>
         public void AfficherHistorique()
         {
-            //lsvChansons.Items.Clear();
-            //lsvChansons.BeginUpdate();
-            //for (int index = m_historique.Count - 1; index >= 0; index--)
-            //{
-            //    Consultation objConsultation = m_historique[index];
-            //    Chanson objChanson = objConsultation.LaChanson;
-            //    ListViewItem objItem = new ListViewItem(objChanson.Artiste);
-            //    objItem.SubItems.Add(objChanson.Titre);
-            //    objItem.SubItems.Add(objChanson.Annee.ToString());
-            //    objItem.SubItems.Add(objConsultation.Délai + "s");
-            //    objItem.SubItems.Add(m_historique.NbConsultationsPourUneChanson(objChanson).ToString());
-            //    lsvChansons.Items.Add(objItem);
-            //}
-            //lsvChansons.EndUpdate();
+            lsvChansons.Items.Clear();
+            lsvChansons.BeginUpdate();
+            for (int index = m_historique.Count - 1; index >= 0; index--)
+            {
+                Consultation objConsultation = m_historique[index];
+                Chanson objChanson = (Chanson)objConsultation.LaChanson;
+                ListViewItem objItem = new ListViewItem(objChanson.Artiste);
+                objItem.SubItems.Add(objChanson.Titre);
+                objItem.SubItems.Add(objChanson.Annee.ToString());
+                objItem.SubItems.Add(objConsultation.Délai + "s");
+                objItem.SubItems.Add(m_historique.NbConsultationsPourUneChanson(objChanson).ToString());
+                lsvChansons.Items.Add(objItem);
+            }
+            lsvChansons.EndUpdate();
         }
         #endregion
 
         private void RaffraîchirNbConsultationsDepuis()
         {
-            //int nombre = m_historique.NbConsultationsDepuisXSecondes((int)numSecondes.Value);
-            //lblNbChansonsDepuis.Text = nombre.ToString();
+            int nombre = m_historique.NbConsultationsDepuisXSecondes((int)numSecondes.Value);
+            lblNbChansonsDepuis.Text = nombre.ToString();
         }
         private void NumSecondes_ValueChanged(object sender, System.EventArgs e)
         {
-            //RaffraîchirNbConsultationsDepuis();
+            RaffraîchirNbConsultationsDepuis();
         }
 
         private void FrmHistorique_FormClosing(object sender, FormClosingEventArgs e)
         {
-            //tmrRaffraîchir.Stop();
+            tmrRaffraîchir.Stop();
         }
 
         private void TmrRaffraîchir_Tick(object sender, System.EventArgs e)
         {
-            //RaffraîchirNbConsultationsDepuis();
-            //for (int index = m_historique.Count - 1; index >= 0; index--)
-            //{
-            //    Consultation objConsultation = m_historique[index];
-            //    Chanson objChanson = objConsultation.LaChanson;
-            //    lsvChansons.Items[index].SubItems[3].Text = objConsultation.Délai + "s";
-            //}
+            RaffraîchirNbConsultationsDepuis();
+            for (int index = m_historique.Count - 1; index >= 0; index--)
+            {
+                Consultation objConsultation = m_historique[index];
+                Chanson objChanson = (Chanson)objConsultation.LaChanson;
+                lsvChansons.Items[index].SubItems[3].Text = objConsultation.Délai + "s";
+            }
         }
     }
 }
--- BaladeurMultiFormats/FrmPrincipal.cs
+++ BaladeurMultiFormats/FrmPrincipal.cs
@@ -14,6 +14,34 @@ namespace BaladeurMultiFormats
 
         #region Propriété : MonHistorique
         public Historique MonHistorique { get; }
+
+        private Baladeur baladeur { get; }
+
+        public Chanson ChansonCourante
+        {
+            get
+            {
+                if (lsvChansons.SelectedIndices.Count == 0)
+                {
+                    return null;
+                }
+                return baladeur.ChansonAt(lsvChansons.SelectedIndices[0]);
+            }
+        }
+
+        public int IndexChansonCourante
+        {
+            get
+            {
+                if (lsvChansons.SelectedIndices.Count == 0)
+                {
+                    return -1;
+                }
+                return lsvChansons.SelectedIndices[0];
+            }
+        }
+
+
         #endregion
         //---------------------------------------------------------------------------------
         #region FrmPrincipal
@@ -22,21 +50,45 @@ namespace BaladeurMultiFormats
             InitializeComponent();
             Text += APP_INFO;
             MonHistorique = new Historique();
-            // À COMPLÉTER...
+
+            baladeur = new Baladeur();
+            baladeur.ConstruireLaListeDesChansons();
+            baladeur.AfficherLesChansons(lsvChansons);
+            lblNbChansons.Text = lsvChansons.Items.Count.ToString();
+            MettreAJourSelonContexte();
+            MonHistorique = new Historique();
+
         }
         #endregion
         //---------------------------------------------------------------------------------
         #region Méthode : MettreAJourSelonContexte
         private void MettreAJourSelonContexte()
         {
-            // À COMPLÉTER...
+            if (ChansonCourante == null)
+            {
+                MnuFormatConvertirVersWMA.Enabled = false;
+                MnuFormatConvertirVersMP3.Enabled = false;
+                MnuFormatConvertirVersAAC.Enabled = false;
+                txtParoles.Text = string.Empty;
+            }
+            else
+            {
+                MnuFormatConvertirVersMP3.Enabled = ChansonCourante.Format != "MP3";
+                MnuFormatConvertirVersWMA.Enabled = ChansonCourante.Format != "WMA";
+                MnuFormatConvertirVersAAC.Enabled = ChansonCourante.Format != "AAC";
+            }
         }
         #endregion
         //---------------------------------------------------------------------------------
         #region Événement : LsvChansons_SelectedIndexChanged
         private void LsvChansons_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // À COMPLÉTER...
+            MettreAJourSelonContexte();
+            if (lsvChansons.SelectedIndices.Count > 0)
+            {
+                txtParoles.Text = ChansonCourante.Paroles;
+                MonHistorique.Add(new Consultation(DateTime.Now, ChansonCourante));
+            }
         }
         #endregion
 
@@ -45,17 +97,26 @@ namespace BaladeurMultiFormats
         private void MnuFormatConvertirVersAAC_Click(object sender, EventArgs e)
         {
             // Vider l'historique car les références ne sont plus bonnes
-            // À COMPLÉTER...
+            MonHistorique.Clear();
+            baladeur.ConvertirVersAAC(IndexChansonCourante);
+            lsvChansons.Items[IndexChansonCourante].SubItems[3].Text = "AAC";
+            MettreAJourSelonContexte();
         }
         private void MnuFormatConvertirVersMP3_Click(object sender, EventArgs e)
         {
             // Vider l'historique car les références ne sont plus bonnes
-            // À COMPLÉTER...
+            MonHistorique.Clear();
+            baladeur.ConvertirVersMP3(IndexChansonCourante);
+            lsvChansons.Items[IndexChansonCourante].SubItems[3].Text = "MP3";
+            MettreAJourSelonContexte();
         }
         private void MnuFormatConvertirVersWMA_Click(object sender, EventArgs e)
         {
             // Vider l'historique car les références ne sont plus bonnes
-            // À COMPLÉTER...
+            MonHistorique.Clear();
+            baladeur.ConvertirVersWMA(IndexChansonCourante);
+            lsvChansons.Items[IndexChansonCourante].SubItems[3].Text = "WMA";
+            MettreAJourSelonContexte();
         }
         #endregion
         //---------------------------------------------------------------------------------
