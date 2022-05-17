b70654b713bec9874c55b1805faf73c267f830b5
Kenji Tagawa <kenjitagawa.kt@gmail.com>
2022-05-17 10:02:54 -0400

    FCT : TODO #15
    Ajout des tests pour la classe Historique

--- BaladeurMultiFormatsTests/UnitTestHistoriqueTODOs.cs
+++ BaladeurMultiFormatsTests/UnitTestHistoriqueTODOs.cs
@@ -7,66 +7,77 @@ namespace BaladeurMultiFormatsTests
     [TestClass]
     public class UnitTestHistoriqueTODOs
     {
-        
+
         #region Tests de la méthode NbConsultationDepuisXSecondes
         // TODO Test E : HistoriqueTestNbConsultationDepuisXSecondesParamNegatifTest
         // Compléter la méthode pour tester le cas où la valeur du délai passé en paramètre est négative
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
         public void HistoriqueTestNbConsultationDepuisXSecondesParamNegatifTest()
         {
             // Arrange : Instancier un objet Historique
-            // À compléter...
-          
+            Historique history = new Historique();
+
             // Act : Appeler la méthode NbConsultationsDepuisXSecondes
-            // À compléter...
-          
-            
+            history.NbConsultationsDepuisXSecondes(-10);
+
             // Assert : Vérifier si la méthode lève une exception IndexOutOfRangeException
-            // À compléter...
+
         }
 
 
         // TODO Test F : HistoriqueTestNbConsultationDepuisXSecondesValideTest
         // Compléter la méthode pour tester le cas valide
+        [TestMethod]
         public void HistoriqueTestNbConsultationDepuisXSecondesValideTest()
         {
             // Arrange : Instancier un objet Historique et y ajouter trois consultations d'une même chansonAAC
             // La première consultation depuis 100 secondes (DateTime.AddSeconds(-100))
             // La deuxième consultation depuis 150 secondes (DateTime.AddSeconds(-150))
             // La troisième consultation depuis 300 secondes (DateTime.AddSeconds(-300))
-            // À compléter...
-          
-            
-            
+            ChansonAAC chansonAAC = new ChansonAAC(@"Chansons/Dancing Queen.AAC");
+            Historique history = new Historique()
+            {
+                new Consultation (DateTime.Now.AddSeconds(-100), chansonAAC),
+                new Consultation (DateTime.Now.AddSeconds(-150), chansonAAC),
+                new Consultation (DateTime.Now.AddSeconds(-300), chansonAAC),
+            };
+
+
+
             // Act : Appeler la méthode NbConsultationsDepuisXSecondes pour calculer le nombre 
             // de chansons consultées depuis 200 secondes.
-            // À compléter...
-           
+            int calculatedConsultations = history.NbConsultationsDepuisXSecondes(200);
+
             // Assert : Vérifier si la méthode retourne le bon nombre de consultations qui est 2 !
-            // À compléter...
-            
+            Assert.AreEqual(calculatedConsultations, 2);
+
 
         }
         #endregion
- 
+
         #region Tests de la méthode NbConsultationsPourUneChanson
         // TODO Test G : HistoriqueTestNbConsultationsPourUneChansonParamNullTest
         // Compléter la méthode pour tester le cas où la chanson passée en paramètre est à null
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
         public void HistoriqueTestNbConsultationsPourUneChansonParamNullTest()
         {
             // Arrange : Instancier un objet Historique
-            // À compléter...
-         
-            
+            Historique history = new Historique();
+
+
             // Act : Appeler la méthode NbConsultationsDepuisXSecondes en lui passant la valeur null
-            // À compléter...
-           
+            history.NbConsultationsPourUneChanson(null);
+
 
             // Assert : Vérifier si la méthode lève une exception ArgumentNullException
-            // À compléter...
+
         }
 
         // TODO Test H : HistoriqueTestNbConsultationsPourUneChansonValideTest
         // Compléter la méthode pour tester le cas valide
+        [TestMethod()]
         public void HistoriqueTestNbConsultationsPourUneChansonValideTest()
         {
             // Arrange : Instancier un objet Historique et un objet ChansonAAC
@@ -75,21 +86,28 @@ namespace BaladeurMultiFormatsTests
             // La deuxième consultation depuis 150 secondes (DateTime.AddSeconds(-150))
             // La troisième consultation depuis 300 secondes (DateTime.AddSeconds(-300))
             // La quatrième consultation depuis 350 secondes (DateTime.AddSeconds(-350))
-            // À compléter...
-          
+            ChansonAAC chansonAAC = new ChansonAAC(@"Chansons/Dancing Queen.AAC");
+            Historique history = new Historique()
+            {
+                new Consultation (DateTime.Now.AddSeconds(-100), chansonAAC),
+                new Consultation (DateTime.Now.AddSeconds(-150), chansonAAC),
+                new Consultation (DateTime.Now.AddSeconds(-300), chansonAAC),
+                new Consultation (DateTime.Now.AddSeconds(-350), chansonAAC),
+            };
+
 
             // Act : Appeler la méthode NbConsultationsDepuisXSecondes pour calculer le nombre 
             // de fois que la chansonAAC a été consultée.
-            // À compléter...
+            int calculatedConsultations = history.NbConsultationsDepuisXSecondes(500);
 
-           
+
             // Assert : Vérifier si la méthode retourne la bon nombre de consultations qui est 4
-            // À compléter...
+            Assert.AreEqual(calculatedConsultations, 4);
 
-           
+
 
         }
-        
+
 
         #endregion
     }
